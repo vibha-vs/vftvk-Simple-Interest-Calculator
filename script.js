a1689be7967a00a9f9fbe05ea7a32b4e56217fe9
vibha-vs <vibhavs217@gmail.com>
2021-07-01 09:19:09 +0530

    Modified Simple Interest Ca;culator

--- script.js
+++ script.js
@@ -4,26 +4,31 @@ function compute()
     //The value of Principal, Rate of Interest and No. of Years is stored in these variables respectively
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    //No.of Years entered is converted into an actual year in the future
-    var year = new Date().getFullYear()+parseInt(years);
+    var days = document.getElementById("days").value;
     //Make sure that the Principal entered is a positive number
     if(document.getElementById("principal").value<=0)
     {
         //Dispaly pop-up alert message if the condition is true
-        alert("Enter a positive number");
+        alert("Please enter a valid Amount");
         document.getElementById("principal").focus();
+    }
+    else if((document.getElementById("rate").value<=0)||(document.getElementById("rate").value>25))
+    {
+        //Display pop-up alert message if the condition is true
+        alert("Please enter a valid Rate of Interest (<25%)");
+        document.getElementById("rate").focus();
+    }
+    else if(document.getElementById("days").value<=0)
+    {
+        //Display pop-up alert message if the condition is true
+        alert("Please enter a valid Time Period");
+        document.getElementById("days").focus();
     }
     else
     {
-    var amount = principal * years * rate /100;
+    var amount = (principal * days * rate /36500);
     //Display the following on computing the result 
-    document.getElementById("result").innerHTML="If you deposit <span class='text-highlight'>"+principal+"</span>,\<br\>at an interest rate of <span class='text-highlight'>"+rate+"%"+"</span>\<br\>You will receive an amount of <span class='text-highlight'>"+amount+"</span>,\<br\>in the year <span class='text-highlight'>"+year+"</span>\<br\>"
+    document.getElementById("result").innerHTML="If the Principal is <span class='text-highlight'>"+"₹"+principal+"</span>\<br\>At an Interest Rate of <span class='text-highlight'>"+rate+"%"+"</span>\<br\>For <span class='text-highlight'>"+days+" days"+"</span>\<br\>The Simple Interest will be <span class='text-highlight'>"+"₹"+amount.toFixed(2)+"</span>\<br\>"
     }
 }
-//Dynamically update the value of the slider
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
+
